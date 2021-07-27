18cb7dd3747e23e05d47735070193fd62ff3184f
Drafenous <rodrigo.roberto.almeida@gmail.com>
2021-07-27 01:34:50 -0300

    changed title and page desc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Rodrigo R. Almeida - Front-end Engineer</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
@@ -17,7 +17,7 @@ function App({ Component, pageProps }: AppProps) {
         />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJ and Styled Components"
+          content="A sample portfolio to list my contact and main technologies"
         />
       </Head>
       <GlobalStyles />
